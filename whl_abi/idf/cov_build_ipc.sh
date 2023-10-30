f0cc44097c094db064dc7004eeeb2f2b7254862f
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2023-10-30 19:04:45 +0530

    Update cov_build_ipc.sh

--- whl_abi/idf/cov_build_ipc.sh
+++ whl_abi/idf/cov_build_ipc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/pkcs11
+pip3 install protobuf
 cd ${WORKSPACE}/abi/bandit/meta-tep-ipc-stack/
 ./bootstrap
 cd ipc_stack/grpc
 mkdir build
-cd build
-export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/pkcs11 
+cd build 
 cmake -DCMAKE_PREFIX_PATH=../../3rd_party/grpc/cmake/build/install_dir -DENABLE_CLIENT=1 -DENABLE_SERVER=1 ../
 make -j
