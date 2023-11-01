199521469c9644555fbf926879fb0cf8c3fd1814
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2023-11-01 08:00:29 +0530

    Update yocto_cov.sh

--- whl_abi/idf/yocto_cov.sh
+++ whl_abi/idf/yocto_cov.sh
@@ -2,8 +2,8 @@
 export http_proxy=http://proxy-dmz.intel.com:912
 export https_proxy=http://proxy-dmz.intel.com:912
 export no_proxy=intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,.local,127.0.0.0/8,134.134.0.0/16
-cd ${WORKSPACE}/abi/meta-tep-build-setup
+cd ${WORKSPACE}/meta-tep-build-setup
 ./setup.sh tep-container --options --manual
-cd ${WORKSPACE}/abi/meta-tep-build-setup/unified_build/tep-container
+cd ${WORKSPACE}/meta-tep-build-setup/unified_build/tep-container
 source ./openembedded-core/oe-init-build-env ./build-tep-container
 bitbake -f -c compile mc:x86-tep-trusted-os-tgl-initramfs:ipc-p11-service-erpc mc:x86-tep-trusted-os-tgl-initramfs:ipc-p11-service-grpc mc:x86-tep-trusted-os-tgl-initramfs:ipc-p11-client-grpc mc:x86-tep-trusted-os-tgl-initramfs:ipc-p11-client-erpc mc:x86-tep-trusted-os-tgl-initramfs:tep-user-config mc:x86-tep-trusted-os-tgl-initramfs:tep-disk-encryption mc:x86-tep-trusted-os-tgl-initramfs:tagentlog mc:x86-tep-trusted-os-tgl-initramfs:trustagent
